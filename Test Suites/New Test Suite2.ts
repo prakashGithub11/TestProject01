4850b58c293b73af3b22f8c7600f10b0ca86b525
prakash.s <prakash.s@LT-LZ-0285.ivygdcad.ivy-support.com>
2025-07-31 00:20:31 +0530

    testops commit test

---
+++ Test Suites/New Test Suite2.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite2</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>2104f948-3c36-4c46-bab0-c617f5da67d5</testSuiteGuid>
+</TestSuiteEntity>
