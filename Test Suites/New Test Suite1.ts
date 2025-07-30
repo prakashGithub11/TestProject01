0725b4fd9a22ecb3980538754c9bd48fbdf7b2ba
prakash.s <prakash.s@LT-LZ-0285.ivygdcad.ivy-support.com>
2025-07-31 00:15:43 +0530

    suite

---
+++ Test Suites/New Test Suite1.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>14707ac6-693a-476c-af1f-33f160d439c5</testSuiteGuid>
+</TestSuiteEntity>
