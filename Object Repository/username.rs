d05c99ad7776d8ad27e5bf245dbcc5a642046af7
prakash.s <prakash.s@LT-LZ-0285.ivygdcad.ivy-support.com>
2025-07-30 20:57:33 +0530

    object commit

---
+++ Object Repository/username.rs
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>username</name>
+   <tag></tag>
+   <elementGuidId>fbe0fa3a-b08f-4909-bcc1-ab86f35bba32</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
